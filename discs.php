738bd7d02edebcfa0e94d5032886168e579c8fc9
MindulaHewadewage <mindufernando@gmail.com>
2023-02-10 14:03:02 +0100

    fixando css

--- discs.php
+++ discs.php
@@ -1,11 +1,13 @@
 <?php
 
 // recuperiamo il file con i dati
-$source_url = __DIR__ . 'data.json';
+$source_url = __DIR__ . '/data.json';
 
 // leggiamo il contenuto del file
 $json_data = file_get_contents($source_url);
 
-
+// dico al server che voglio scrivere in json
 header('Content-Type:application/json');
-echo json_encode($discs);
+
+// converto in json
+echo $json_data;
